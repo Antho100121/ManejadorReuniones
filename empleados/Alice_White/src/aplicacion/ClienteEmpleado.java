4a38846a1a3a77bcb5393e1780fad847071952f1
KeomaQuiroga <keomaquiroga@gmail.com>
2025-05-03 21:42:34 -0500

    Estructura aplicacion Alice_White

--- empleados/Alice_White/src/aplicacion/ClienteEmpleado.java
+++ empleados/Alice_White/src/aplicacion/ClienteEmpleado.java
@@ -1,5 +1,29 @@
 package aplicacion;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import modelo.Reunion;
+
 public class ClienteEmpleado {
-    
+    //atributos
+    private String servidorHost;
+    private int servidorPuerto;
+
+    //constructor
+    ClienteEmpleado(String host, int puerto){
+        this.servidorHost = host;
+        this.servidorPuerto = puerto;
+    }
+
+    //metodos
+    public void enviarCambios(Reunion r) {
+        try (Socket socket = new Socket(servidorHost, servidorPuerto);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+            // out.println(r.serialize());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- empleados/Alice_White/src/aplicacion/ServidorEmpleado.java
+++ empleados/Alice_White/src/aplicacion/ServidorEmpleado.java
@@ -1,5 +1,39 @@
 package aplicacion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import modelo.Reunion;
+
 public class ServidorEmpleado {
-    
+    private int puerto;
+    private ManejadorReuniones manejador;
+
+    public ServidorEmpleado(int puerto, ManejadorReuniones manejador) {
+        this.puerto = puerto;
+        this.manejador = manejador;
+    }
+
+    public void escuchar() {
+        try (ServerSocket serverSocket = new ServerSocket(puerto)) {
+            while (true) {
+                try (Socket cliente = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()))) {
+
+                    String linea = in.readLine();
+                    // Reunion r = Reunion.deserialize(linea);
+                    // manejador.modificarReunion(r);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
